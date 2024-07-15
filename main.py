5e33fffad50e1450edc16fbbd0c5635d3e21ff60
Batouchan Omer <batouchan.omer@gmail.com>
2024-07-15 15:20:37 +0300

    Removed unused code

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
 from src.cli.app import app
 
 
-def display_intro():
-
-    print("")
-    print("complex systems and patterns. ")
-    print("")
-    print("Let's get started!\n")
-    print("\n\n\n")
-    print("\n\n\n")
-
-
 def main():
     app()
-
-
 
 
 if __name__ == "__main__":
